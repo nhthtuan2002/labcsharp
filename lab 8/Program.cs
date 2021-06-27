db8cdc7804057aa3b140e419501bc4728cccd747
NguyenHoangTuan <tuannhth2009020@fpt.edu.vn>
2021-06-27 11:44:00 +0700

    a

---
+++ lab 8/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab_8
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var temperatures = new List<int>
+            {
+                1, 200, 15, 50
+            };
+
+            var dem = 0;
+            foreach (var n in temperatures)
+            {
+                if(n >= 25) {
+                    dem += 1;
+                }
+            }
+            Console.WriteLine("Number of temperatures >= 25 degrees: {0}", dem);
+            
+            var dem2 = GreaterCount(temperatures, 25);
+            
+            Console.WriteLine("Number of elements of the list >= min: {0}", dem2);
+        }
+
+        private static int GreaterCount(List<int> list, double min)	
+        { 
+            var dem2 = 0;
+            foreach (var n in list)
+            {
+                if(n >= min) {
+                    dem2 = dem2 + 1;
+                }
+            }
+            return dem2;
+        }
+    }
+}
---
+++ lab8 _2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab8__2
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            IEnumerable<double> able = new List<double>();
+            var dem = GreaterCount(able, 0);
+            Console.WriteLine("Number of elements of the list >= min: {0}", dem);
+        }
+
+        private static double GreaterCount(IEnumerable<double> able, double min)
+        {
+            double dem = 0;
+            foreach (var e in able)
+            {
+                if (e>=min)
+                {
+                    dem += 1;
+                }
+            }
+            return dem;
+        }
+    }
+}
